c1d6f583967c9385f7a6ca95ab75040eef374334
subscriptionflow-developer <68118219+subscriptionflow-developer@users.noreply.github.com>
2021-08-17 19:10:00 +0500

    ReplaceBlock fix

--- src/PhpWord/TemplateProcessor.php
+++ src/PhpWord/TemplateProcessor.php
@@ -836,22 +836,13 @@ class TemplateProcessor
      * @param string $blockname
      * @param string $replacement
      */
-    public function replaceBlock($blockname, $replacement)
-    {
-        $matches = array();
-        preg_match(
-            '/(<\?xml.*)(<w:p.*>\${' . $blockname . '}<\/w:.*?p>)(.*)(<w:p.*\${\/' . $blockname . '}<\/w:.*?p>)/is',
-            $this->tempDocumentMainPart,
-            $matches
+     public function replaceBlock($blockname, $replacement) 
+     {
+        $this->tempDocumentMainPart = preg_replace(
+          '/(\${' . $blockname . '})(.*?)(\${\/' . $blockname . '})/is',
+          $replacement,
+          $this->tempDocumentMainPart
         );
-
-        if (isset($matches[3])) {
-            $this->tempDocumentMainPart = str_replace(
-                $matches[2] . $matches[3] . $matches[4],
-                $replacement,
-                $this->tempDocumentMainPart
-            );
-        }
     }
 
     /**
